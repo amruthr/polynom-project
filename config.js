6cfd35b3350de1a64e63314a09c9fdb4d2149d0e
amruthr <34576243+amruthr@users.noreply.github.com>
2020-05-02 10:34:44 +0530

    fixed security vulnerability

--- config.js
+++ config.js
@@ -3,16 +3,4 @@ module.exports = {
   secret: 'MY SECRET',
   DB_URI_DEV: 'mongodb://localhost:27017/tc',
   DB_URI: process.env.DB_URI,
-  service_account: {
-    "type": "service_account",
-    "project_id": "travelcrest2020",
-    "private_key_id": "9fa8dacbc500e3deb62e39ca6fef6a40f78d44f1",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDa364xnD+/RTPe\neutcfTTRbAoFUrEBvEiS03ALSxXUp8bavs+Ox5+C5fSFcVoWCMpE6QBnlFT5292F\nJVExs/w7cnE37UamjxKpVyaFkRRugbNh46h4BwZeQEs5X9YhA2tvQX0DYEBANRIg\nNfr1Uqci86gzW6ShjhsvHYhdb0qw8RDY+LlNE5hHfK0hkzhSyXlh/zGmX9tpVkw+\nS4q5WgUQRYISRg0Y6ynZ9IGKGblw0CZWpEdvIeGxgLuwR+Buh3jef+fw6f+rdqRN\nmEYPg0+f031sAk+9WPrbhiIIvWYfrsSvkhZ9ydajTb9rbvqCnNzFLnPOlqcr+hQ5\nRFSHiiZHAgMBAAECggEAD7fhmCF6+H79Rxx3u5goS2PhYll6ab/+TEEP+lellTSJ\nVNtAJ6ZRX8Ko6U5nJE70S7eZ9lZSNfFiurWf25xr4E8LL3OET+DVe8DM/Oma2IbK\n/ZwuVJRYkjs2y8OEqrFGrWI8Gpp2csX868BEqgqKAzoRWcLak1mTvTH3QZKFACjl\nt8SSefTwxs8MPPhtU7aSXUGlYUCr46G4LXeGOVhI+9aEdDIVRerKDyweD1339UYH\nhcON9qmITTWWW+VKlJEuhWKs5WajKPx+rsurvm4k1kjrHSvzL0sNU4/3m9TdiX/7\nAv7m70G5IiIybrtMrzCfblcBMPBRGkIEuIbP4TlcPQKBgQD7bzuhpOUMNYaDzRRv\ncnH9mKAQx7oP450E5r3cSn5pts12TNQi6UFl6ohU73zHi4IdV2DucFbqgoMJNOJi\nebvEHMyUlLFHYXaD7iW0ULAzm7/x94027CiFDNm+JobD+WVxNUHLeExbTrQDgckJ\njYi140q7CySwMDbbYaaJ0kYizQKBgQDe2RedtOMXcDTtogX2QnHcIC6aQL4L6DI0\nUlIcXj2F92YGrIfyErkGGzL+pJC+Q4ORnbSN038P8kR1ZlaW9RuaudtzxjRIlitV\nF0azB0E9jZFOecHaY1tzhD98geKTjWoR6JKaC66voxJKzn5ZCeT8mA4Cr7oyPYtO\nrbRzmI91YwKBgHzysfeKMij3EHm/GECp5uvXRPQRzOzId+dPo1otytRN+5ymYWVX\n9R/ohQdwW9nuAiMCoTDcstSnNucEWNwHEsBKb4AGxHJshLDbT/Fjh6yd4a0lEBAT\nG6/CyxIrox0QetDfXmr2xTs6yUpU3Mragu2gniqFRx4nV5t903z5+dChAoGAGRZz\nlCuTDUZyjvYYmbqtOszcsjzrqEeBr2Y9HDh30Kcjc0jlHRmCfu989DHuoZzUKQaK\nFxViTpdFobndsazyaJ7X19bOeAV/51pxeSIt9BIF88G9UyqwVvbids/ghB/9evql\n1nXOOrOvEYDhtIBqqQ0tNpw16AYJstfXYtQgAmUCgYEAgK/IrQAMa531IpCxCq3h\naO6SqceNfAmUwb4RNL98WP4G77/mt0lfpcWezqiVryj3g9MweoHf1C0NqQ9Pbz53\nNCp6fbm1bOmhN84HplJ0nej2u2V7beT7aX3oCa11j3CsWj8r4Sce5NJJK8Fgnz4B\nRK4IRZuDjnCmKslI+rrdPE8=\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-aod16@travelcrest2020.iam.gserviceaccount.com",
-    "client_id": "112008968719426042910",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-aod16%40travelcrest2020.iam.gserviceaccount.com"
-}
 };
